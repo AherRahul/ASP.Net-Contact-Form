9219a4ab1dfaa331a9c28e5d60bd8398b35ccf23
aherrahul1995 <43370315+aherrahul1995@users.noreply.github.com>
2020-04-12 16:58:35 +0530

    first commit

---
+++ WebApplication3/Default.aspx
@@ -0,0 +1,75 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication3.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
+    <link href="StyleSheet.css" rel="stylesheet" />
+
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <div class="container">
+                <div class="contact-parent">
+                    <div class="contact-child child1">
+
+                        <p>
+                            <i class="fas fa-map-marker-alt"></i>Address
+                            <br />
+                            <span>110  Ash Lane,
+                            <br />
+                            YARROW FEUS, TD7 0XL</span>
+                        </p>
+                        <p>
+                            <i class="fas fa-phone-alt"></i>Let's talk
+                            <br />
+                            <span>070 8826 9269</span>
+                        </p>
+                        <p>
+                            <i class="far fa-envelope"></i>General Support
+                            <br />
+                            <span>andreea@andreeabunget.co.uk</span>
+                        </p>
+
+                    </div>
+                    <div class="contact-child child2">
+                        <div class="inside-contact">
+                            <h2>Contact Us</h2>
+                            <h3>
+                                <asp:Label ID="confirm" runat="server" Text=""></asp:Label>
+                            </h3>
+                            <p>
+                                <asp:Label ID="Label1" runat="server" Text="First Name *"></asp:Label></p>
+                            <asp:TextBox ID="txt_first_name" runat="server" Required="required"></asp:TextBox>
+                                
+                                <asp:Label ID="Label6" runat="server" Text="Last Name *"></asp:Label></p>
+                            <asp:TextBox ID="txt_last_name" runat="server" Required="required"></asp:TextBox>
+                            <p>
+                                <asp:Label ID="Label2" runat="server" Text="Email *"></asp:Label></p>
+                            <asp:TextBox ID="txt_email" runat="server" Required="required"></asp:TextBox>
+
+                            <p>
+                                <asp:Label ID="Label3" runat="server" Text="Phone *"></asp:Label></p>
+                            <asp:TextBox ID="txt_phone" runat="server" Required="required"></asp:TextBox>
+
+                            <p>
+                                <asp:Label ID="Label4" runat="server" Text="Cake Type *"></asp:Label></p>
+                            <asp:TextBox ID="txt_subject" runat="server" Required="required"></asp:TextBox>
+
+                            <p>
+                                <asp:Label ID="Label5" runat="server" Text="Message *"></asp:Label></p>
+                            <asp:TextBox ID="txt_message" runat="server" Required="required" TextMode="MultiLine" Rows="4"></asp:TextBox>
+
+                            <asp:Button ID="btn_send" runat="server" Text="Send" OnClick="btn_send_Click"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication3/Default.aspx.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Drawing;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+
+namespace WebApplication3
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btn_send_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // credentials
+                var from = "thecakeshop369@gmail.com";
+                var to = "thecakeshop369@gmail.com";
+                const string Password = "C@keshop123";
+
+                // email subject
+                string mail_subject = txt_subject.Text.ToString();
+
+                //email body
+                string mail_message = "From: " + txt_first_name.Text + " " + txt_last_name.Text + "\n";
+                mail_message += "Email: " + txt_email.Text + "\n";
+                mail_message += "Order For Category: " + txt_subject.Text + "\n";
+                mail_message += "Phone: " + txt_phone.Text + "\n";
+                mail_message += "Message: \n" + txt_message.Text + "\n";
+
+
+                // mail smtp 
+                var smtp = new SmtpClient();
+                {
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+                    smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    smtp.Credentials = new NetworkCredential(from, Password);
+                    smtp.Timeout = 20000;
+                }
+
+                smtp.Send(from, to, mail_subject, mail_message);
+
+                Confirm();
+
+                confirm.Text = "Thanks for contacting us! We will be in touch with you shortly";
+
+                txt_subject.Text = "";
+                txt_first_name.Text = "";
+                txt_last_name.Text = "";
+                txt_email.Text = "";
+                txt_phone.Text = "";
+                txt_message.Text = "";
+
+
+            }
+
+            // error message 
+            catch (Exception)
+            {
+                confirm.Text = "<p> Oh no! <br> Something went wrong while sending your message ";
+                confirm.ForeColor = Color.Red;
+            }
+        }
+
+        // send confirmation mail back to client
+        private void Confirm()
+        {
+            // pass text input to variable
+            string ToEmail = txt_email.Text.Trim();
+            string UserName = txt_first_name.Text + " " + txt_last_name.Text;
+            string subject2 = txt_subject.Text;
+
+            MailMessage mailMessage = new MailMessage("thecakeshop369@gmail.com", ToEmail);
+
+
+
+            StringBuilder sbEmailBody = new StringBuilder();
+            sbEmailBody.Append("<img src='https://tonycreators.com/wp-content/uploads/2020/03/Tony-Creators-Logo-e1585059175346.png' alt=logo width='150';/>");
+            sbEmailBody.Append("<br/><br/>");
+            sbEmailBody.Append("Dear " + UserName + ",<br/><br/>");
+            sbEmailBody.Append("Thank you for reaching us!<br/>");
+            sbEmailBody.Append("We received you order regarding " + subject2 + " cake category<br/>");
+            sbEmailBody.Append("We will be back to you within 2 working days. It might take a little longer on evenings and weekends but we want" +
+                " you to know that we are doing our best to get back to you as soon as possible");
+            sbEmailBody.Append("<br/><br/><br/>");
+            sbEmailBody.Append("Sincerely, <br/>");
+            sbEmailBody.Append("<b>The Cake Shop<br>Mob No: 1234567890</b>");
+
+
+            // convert the html tag
+            mailMessage.IsBodyHtml = true;
+
+            mailMessage.Body = sbEmailBody.ToString();
+            mailMessage.Subject = "Re: Thank you for your email";
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+
+            smtpClient.Credentials = new System.Net.NetworkCredential()
+            {
+                UserName = "thecakeshop369@gmail.com",
+                Password = "C@keshop123"
+            };
+
+
+            smtpClient.EnableSsl = true;
+            smtpClient.Send(mailMessage);
+        }
+
+    }
+}
